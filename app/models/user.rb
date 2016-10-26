eb2df5053e6851e541b21ebc85325d7140a31034
shishir kumar <shishirsharad@yahoo.com>
2016-10-26 17:41:02 +0530

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+attr_accessor :remember_token
+
 	             # whole statement is similiar to writing email.downcase!
 	before_save {self.email= email.downcase} # self.email.downcase can be written as email.downcase
 	validates(:name, presence: true, length: {maximum: 50})   # it's a method
@@ -7,11 +10,31 @@ class User < ApplicationRecord
 format: {with: VALID_EMAIL_REGEX } , uniqueness: true ,uniqueness: {case_sensitive: false} )  # a method
        has_secure_password
 
+
+
 validates(:password, presence: true, length: {minimum: 6})
 
 def User.digest(string)
 	cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
 	BCrypt::Password.create(string, cost: cost)
+end
+
+def User.new_token
+	SecureRandom.urlsafe_base64
+end
+
+def remember
+	self.remember_token = User.new_token
+	update_attribute(:remember_digest, User.digest(remember_token))
+end
+
+def authenticated?(remember_token)
+	return false if remember_digest.nil?
+	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+end
+
+def forget
+	update_attribute(:remember_digest, nil)
 end
 
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -21,6 +21,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 		delete logout_path
 		assert_not is_logged_in?
 		assert_redirected_to root_url
+		
+		#stimulate a user clicking a logout in  a second window
+		
+		delete logout_path		
 		follow_redirect!
 		assert_select "a[href=?]",  login_path
 		assert_select "a[href=?]", logout_path, count: 0
@@ -46,6 +50,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   	assert_not flash.empty?
   	get root_path
   	assert flash.empty?
+  end
+  
+  test "login with remembering" do
+  	log_in_as(@user, remember_me: '1')
+  	assert_equal cookies['remember_token'], assigns(:user).remember_token
+  end
+  
+  test "login without remembering" do
+  	log_in_as(@user, remember_me: '0')
+  	assert_nil cookies['remember_token']
   end
   
 end
